34bc952f45de16aeb8ea1585c45c7a6c7ecdac62
micty <79230533@qq.com>
2018-01-23 10:36:44 +0800

    1.5.0

---
+++ build/1.5.0/index.js
@@ -0,0 +1,222 @@
+﻿
+var $ = require('./f/defineJS');
+var Args = require('./args');
+var _require = require;             //node.js 原生的 require，备份一下。
+
+
+module.exports = {
+    /**
+    * 解析和获取命令行 `node watch` 或 `node build` 后面的参数。
+    * 参数：
+    *   type: 'watch' | 'build',
+    * 返回：
+    *   args = {
+    *       pack: false,    //是否使用了 `pack` 选项。　如果是则使用独立打包的方式。
+    *       action: '',     //编译完成后要执行的动作。 如 `open` 或 `qr`。
+    *       value: '',      //要传递给 action 的值。 如 `localhost`、`450`。
+    *   };
+    */
+    getArgs: function (type) {
+
+        switch (type) {
+            case 'watch':
+                return Args.watch();
+
+            case 'build':
+                return Args.build();
+
+            default:
+                console.log('无法识别的参数 type 值: '.red, type);
+                throw new Error();
+        }
+    },
+
+    /**
+    * 启动程序。
+    * 参数：
+    *   factory: fn,    //工厂函数。
+    */
+    launch: function (factory) {
+        $.config({
+            'base': __dirname,
+            'modules': [
+                'lib/',
+                'modules/',
+            ],
+        });
+
+        $.launch(function (require, module, exports) {
+            var Console = require('Console');
+            console.log = Console.log;
+
+            Console.config({
+                file: 'console.log',
+                timestamp: true,
+            });
+
+            factory && factory(require, module, exports);
+
+        });
+    },
+
+
+    /**
+    * 启动和初始化程序，创建一个网站实例。
+    * 参数：
+    *   defaults: {},
+    *   done: fn,
+    */
+    create: function (defaults, done) {
+        var begin = new Date();
+
+        module.exports.launch(function (require, module, exports) {
+            var $ = require('$');
+            var Emitter = $.require('Emitter');
+            var WebSite = require('WebSite');
+            var MasterPage = require('MasterPage');
+            var Console = require('Console');
+            var Watcher = require('Watcher');
+            var Css = require('Css');
+            var Js = require('Js');
+
+            var emitter = new Emitter();
+            var website = new WebSite(defaults);
+            var templates = defaults.templates || {};
+            var tags = defaults.tags;
+
+
+            //设置相应模块的默认配置。
+            Console.config(defaults.console);
+            Watcher.config(defaults.watcher);
+
+            if (tags) {
+                MasterPage.config({ 'tags': tags, });
+            }
+
+            if (templates['css']) {
+                Css.config({ 'sample': templates['css'], });
+            }
+
+            if (templates['js']) {
+                Js.config({ 'sample': templates['js'], });
+            }
+
+            
+
+            done(require, module, {
+                'website': website,
+                'begin': begin,
+                'emitter': emitter,
+            });
+
+        });
+    },
+
+
+    /**
+    * 使用常规方式进行编译和监控。
+    * 参数：
+    *   config = {
+    *       args: {},
+    *       defaults: {},
+    *       options: {},
+    *       done: fn,
+    *   };
+    */
+    watch: function (config) {
+        var args = config.args;
+        var defaults = config.defaults;
+        var options = config.options;
+        var done = config.done;
+
+        module.exports.create(defaults, function (require, mocule, exports) {
+            var emitter = exports.emitter;
+            var website = exports.website;
+            var begin = exports.begin;
+
+            //监控完成后。
+            website.on('watch', function () {
+                var time = new Date() - begin;
+                console.log('耗时'.bgGreen, time.toString().cyan, 'ms');
+
+
+                emitter.on({
+                    'open': function (host) {
+                        if (host == '.') { //提供快捷方式。
+                            host = 'localhost';
+                        }
+
+                        website.open({ 'host': host, });
+                    },
+
+                    'qr': function (width) {
+                        website.openQR({ 'width': width, });
+                    },
+                });
+
+                emitter.fire(args.action, [args.value]);
+                done && done(require, mocule, exports);
+            });
+
+            //开启监控。
+            website.watch(options);
+        });
+    },
+
+    /**
+    * 使用常规方式进行构建。
+    * 参数：
+    *   config = {
+    *       args: {},
+    *       defaults: {},
+    *       options: {},
+    *       done: fn,
+    *   };
+    */
+    build: function (config) {
+        var args = config.args;
+        var defaults = config.defaults;
+        var options = config.options;
+        var done = config.done;
+
+
+        module.exports.create(defaults, function (require, module, exports) {
+            var emitter = exports.emitter;
+            var website = exports.website;
+            var begin = exports.begin;
+
+            //构建完成后。
+            website.on('build', function () {
+                var time = new Date() - begin;
+
+                console.log('耗时'.bgGreen, time.toString().cyan, 'ms');
+
+
+                emitter.on({
+                    'open': function (host) {
+                        if (host == '.') { //提供快捷方式。
+                            host = 'localhost';
+                        }
+
+                        website.open({ 'host': host, });
+                    },
+
+                    'qr': function (width) {
+                        website.openQR({ 'width': width, });
+                    },
+                });
+
+                emitter.fire(args.action, [args.value]);
+                done && done(require, module, exports);
+            });
+
+            //开始构建。
+            website.build(options);
+
+        });
+    },
+
+};
+
+
+
---
+++ build/1.5.0/modules/block/master-block/MasterBlock/Parser.js
@@ -0,0 +1,96 @@
+﻿
+/**
+* 
+*/
+define('MasterBlock/Parser', function (require, module, exports) {
+    var Patterns = require('Patterns');
+    var MasterPage = require('MasterPage');
+
+
+
+    return exports = {
+
+        /**
+        * 
+        */
+        parse: function (meta) {
+            //解析出来的新列表，尽量复用之前创建的实例。
+            var news = [];                      //需要新建的。
+            var olds = [];                      //可以复用的。
+            var file$master = meta.file$master;     //当前集合。
+            var old$master = meta.old.file$master;  //旧集合。
+            var files = Patterns.getFiles(meta.patterns);
+
+            var list = files.map(function (file) {
+                return {
+                    'file': file,
+                    'master': null,
+                    'isOld': false,
+                    'key$output': {},   //针对编译中不同的 options 的缓存结果。 key = JSON.string(options);
+                    'output': null,     //缓存最近一次的编译结果。
+                };
+            });
+
+
+            list.forEach(function (item) {
+                var file = item.file;
+                var master = old$master[file];
+
+                //尚未存在，则先添加到新建列表里。
+                if (!master) {
+                    news.push(item);
+                    return;
+                }
+
+                //复用过来的实例，不需要重新解析。
+                console.log('复用'.bgGreen, file.gray);
+                item.isOld = true;
+                olds.push(file);
+                item.master = file$master[file] = master;
+            });
+
+
+            //有可能同一个文件名给引用了多次，这里也对应为一个实例。
+            news.forEach(function (item) {
+                var file = item.file;
+                var master = file$master[file];
+
+                if (!master) {
+                    master = new MasterPage({
+                        'file': file,
+                        'htdocs': meta.htdocs,
+                        'css': meta.css,
+                        'dest': meta.dest,
+                        'excludes': meta.excludes,
+                        'delay': 100,   //这里不能设为 0，因为 less、js、html 各种资源有可能同时引起触发。
+                    });
+
+                    //新建的要解析。
+                    master.parse();
+                }
+
+                item.master = file$master[file] = master;
+            });
+
+
+            //释放备份中没有复用到的实例。
+            Object.keys(old$master).forEach(function (file) {
+                var master = old$master[file];
+                delete old$master[file];
+
+                //不在可以复用的集合里，则销毁。
+                if (!olds.includes(file)) {
+                    master.destroy();
+                }
+            });
+
+            return list;
+
+        },
+
+      
+    };
+    
+});
+
+
